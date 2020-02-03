d861b1056bd8c63c8a8bc61d7a33ffffb5fe9c00
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-02-03 21:31:46 +0200

    futures

---
+++ futures.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+import 'dart:async';
+
+void main() {
+
+  String path = Directory.current.path + "/test.txt";
+  print('Appending to ${path}');
+  
+  File file = File(path);
+  
+  Future<RandomAccessFile> f = file.open(mode: FileMode.APPEND);
+  
+  f.then((RandomAccessFile raf) {
+    print('File has been opened!');
+    
+    raf.writeString('Hello World').then((value) {
+      print('file has been append!');
+    }).catchError(() => print('An error occured')).whenComplete(() => raf.close());
+  });
+
+  print('***** The end');
+}
