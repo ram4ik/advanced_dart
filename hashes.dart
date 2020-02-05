6d4817a5620a79898128f2da7f25b87ad21dd2ba
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-02-05 20:02:42 +0200

    hashes

---
+++ hashes.dart
@@ -0,0 +1,16 @@
+import 'dart:typed_data';
+import 'dart:convert';
+import 'package:pointycastle/pointycastle.dart';
+
+void main() {
+
+  Digest digest = new Digest("SHA-256");
+
+  String value = 'Hello World';
+
+  Uint8List data = new Uint8List.fromList(utf8.encode(value));
+  Uint8List hash = digest.process(data);
+
+  print(hash);
+  print(base64.encode(hash));
+}
