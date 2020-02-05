a5d52c802792a568c199da83a1e4163eab3cb715
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-02-05 19:27:31 +0200

    gzip vs zlib

---
+++ gzip_vs_zlib.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+import 'dart:convert';
+
+void main() {
+
+  int zlib = testCompress(ZLIB);
+  int gzip = testCompress(GZIP);
+
+  print('zlib = ${zlib}');
+  print('gzip = ${gzip}');
+
+}
+
+String generateData() {
+  String data = '';
+
+  for(int i = 0; i < 1000; i++) {
+    data = data + 'Hello World\r\n';
+  }
+
+  return data;
+}
+
+int testCompress(var codec) {
+  String data = generateData();
+
+  List original = utf8.encode(data);
+  List compressed = codec.encode(original);
+  List decompressed = codec.decode(compressed);
+
+  print('Testing ${codec.toString()}');
+  print('Original ${original.length}');
+  print('Compressed ${compressed.length}');
+  print('Decompressed ${decompressed.length}');
+
+  print('');
+
+  String decoded = utf8.decode(decompressed);
+  assert(data == decoded);
+
+  return compressed.length;
+}
